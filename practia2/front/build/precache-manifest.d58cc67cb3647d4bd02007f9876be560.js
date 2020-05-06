dafee01d75903349c5cb7beb26ad6ba264dae99e
Byron Lopez <bj@localhost.localdomain>
2020-05-06 11:40:38 -0600

    uis front?

---
+++ practia2/front/build/precache-manifest.d58cc67cb3647d4bd02007f9876be560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e27cd521ce610694ba144c85bdbe24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7faae3833562e858888",
+    "url": "/static/css/main.5f724c1b.chunk.css"
+  },
+  {
+    "revision": "ae62ac01a9e10f46bd78",
+    "url": "/static/js/2.33111a29.chunk.js"
+  },
+  {
+    "revision": "f7faae3833562e858888",
+    "url": "/static/js/main.747a73cc.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  }
+]);
